866bcab9ab30ce6dc04f46428963035df14dfcc8
Gabriel Urso Santana Reyes <briel@MacBook-Pro-de-Gabriel.local>
2015-11-09 14:20:19 +0100

    final

--- Juego de Memoria.playground/Contents.swift
+++ Juego de Memoria.playground/Contents.swift
@@ -2,4 +2,23 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var str = "Bienvenido al Juego de Memoria"
+
+var elementos : [Int]=[]
+for i in 0...100{
+    elementos.append(i)
+}
+for i in elementos{
+    if i%5==0{
+        print("# \(i) Bingo!!!")
+    }
+    if i%2 == 0{
+        print("# \(i) par!!!")
+    }
+    if i%2 != 0{
+        print("# \(i) impar!!!")
+    }
+    if i>=30 && i <= 40{
+        print("# \(i) VIVA SWIFT!!!")
+    }
+}
